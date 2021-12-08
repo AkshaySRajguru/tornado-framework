c52942d7e9192c893decf4ea313b3e42d17db646
Akshay Rajguru <akshaygururaj141@gmail.com>
2021-12-08 20:07:16 +0530

    resource parameter get request

--- index.py
+++ index.py
@@ -25,11 +25,23 @@ class QueryParamRequestHandler(tornado.web.RequestHandler):
             self.write(f"{num} is not a valid integer.")
 
 
+class ResourceParamRequestHandler(tornado.web.RequestHandler):
+    """
+    Parameters passed from regex will be arguments for HTTP methods.
+    e.g. studentName, courseId for get method
+    Accessing resource parameter from url: http://localhost:8882/students/akshay/12
+    Output: Welcome akshay you are viewing course 12
+    """
+    def get(self, studentName, courseId):
+        self.write(f"Welcome {studentName} you are viewing course {courseId}")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", BasicRequestHandler),
         (r"/fruits", ListFruitsRequestHandler),
-        (r"/isEven", QueryParamRequestHandler)
+        (r"/isEven", QueryParamRequestHandler),
+        (r"/students/([a-z]+)/([0-9]+)", ResourceParamRequestHandler)
     ])
 
     port = 8882
